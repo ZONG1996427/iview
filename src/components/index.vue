08b73b8a12685fefcf8f0ddebb9a28914fba6675
ZONG1996427 <qqq926634>
2019-10-06 13:42:53 +0800

    测试2

--- src/components/index.vue
+++ src/components/index.vue
@@ -58,7 +58,7 @@
           <Layout :style="{padding: '0 24px 24px'}">
             <Breadcrumb :style="{margin: '24px 0'}">
               <BreadcrumbItem>Home</BreadcrumbItem>
-              <BreadcrumbItem>Components</BreadcrumbItem>
+              <BreadcrumbItem>Components16161</BreadcrumbItem>
               <BreadcrumbItem>Layout</BreadcrumbItem>
             </Breadcrumb>
             <Content :style="{padding: '24px', minHeight: '280px', background: '#fff'}">
